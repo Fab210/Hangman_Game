ce073888a5c0102fc018126711ed4ff40338d23c
Your Name <you@example.com>
2021-04-16 00:09:50 +0200

    get word from api working

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,86 @@
 var wordToFind = "";
+var wordObject;
 var tryToFindLetter = 5;
 generateWordTofind();
 //searchLetterInWord();
+//getwordFromApi();
 generateButtonsABC();
 
-function generateWordTofind() {
-    document.getElementById("numbersOfTry").innerHTML = tryToFindLetter;
-  this.wordToFind = "lolsdfssrtzdfsdfsdfs sd";
+/*async function getwordFromApi() {
+  let apiResponse = await fetch('https://random-words-api.vercel.app/word')
+
+  let wordFromApi = await apiResponse.json();
   debugger
-  this.wordToFind = this.wordToFind.toUpperCase();
+  wordToFind = await wordFromApi[0].word
+  generateWordTofind()
+}*/
+
+function generateWordTofind() {
+  document.getElementById("numbersOfTry").innerHTML = tryToFindLetter;
+  debugger
+  //let result = await getwordFromApi();
+
+
+
+
+
+  //let wordFromApi = await apiResponse.json();
+
+  //this.wordToFind = await wordFromApi[0].word
+
+  //wordToFind = data[0].word
+  debugger
+  wordToFind = wordToFind.toUpperCase();
   var wordInarray = wordToFind.split("");
 
-  for (let i = 0; i < wordInarray.length; i++) {
-    const element = wordInarray[i];
 
-    document.addEventListener(
-      "DOMContentLoaded",
-      function () {
-        var generateLetters = document.createElement("div");
-        
 
-        //generateLetters.id = "letter " + element;
-        generateLetters.innerText = element;
-        generateLetters.className = "letter_"+ element + ' ' +'hideLetter';
-        generateLetters.style.borderBottomStyle = "solid";
-        generateLetters.style.borderBottomWidth = "1px";
-        generateLetters.style.borderBottomColor = "#000";
-        generateLetters.style.color = "#fff";
-        generateLetters.style.marginLeft = "5px";
-        generateLetters.style.paddingLeft = "5px";
-        generateLetters.style.width= "20px";
+  document.addEventListener(
+    "DOMContentLoaded",
+    function () {
+      fetch('https://random-words-api.vercel.app/word')
+        .then(response => response.json())
+        .then(data => renderQuotes(data))
+
+
+      function renderQuotes(data) {
+        debugger
+        wordToFind = data[0].word
+        wordToFind = wordToFind.toUpperCase();
+        var wordInarray = wordToFind.split("");
         
-        generateLetters.style.paddingRight = "5px";
-        var wordToFindDiv = document.getElementById("wordToFind");
-        wordToFindDiv.appendChild(generateLetters);
-      },
-      false
-    );
-  }
+        for (let i = 0; i < wordInarray.length; i++) {
+          const element = wordInarray[i];
+
+          var generateLetters = document.createElement("div");
+
+
+          //generateLetters.id = "letter " + element;
+          generateLetters.innerText = element;
+          generateLetters.className = "letter_" + element + ' ' + 'hideLetter';
+          generateLetters.style.borderBottomStyle = "solid";
+          generateLetters.style.borderBottomWidth = "1px";
+          generateLetters.style.borderBottomColor = "#000";
+          generateLetters.style.color = "#fff";
+          generateLetters.style.marginLeft = "5px";
+          generateLetters.style.paddingLeft = "5px";
+          generateLetters.style.width = "20px";
+
+          generateLetters.style.paddingRight = "5px";
+          var wordToFindDiv = document.getElementById("wordToFind");
+          wordToFindDiv.appendChild(generateLetters);
+        }
+      }
+    }
+
+
+
+
+  );
+
+
+
+
 }
 
 function searchLetterInWord(letter) {
@@ -48,26 +91,33 @@ function searchLetterInWord(letter) {
   if (tryToFindLetter != 1) {
     var searchLetter = wordToFind.includes(letter);
     if (searchLetter == true) {
-        debugger
+      debugger
       alert("you find a letter !");
-      var x = document.getElementsByClassName("letter_"+ letter);
+      var x = document.getElementsByClassName("letter_" + letter);
       for (i = 0; i < x.length; i++) {
         x[i].style.color = "#000";
       }
-     
+
     } else {
       alert("no such letter found");
       debugger
       tryToFindLetter--;
       document.getElementById("numbersOfTry").innerHTML = tryToFindLetter;
-     
+      document.getElementById("letter " + letter).disabled = true;
 
-      
+
+
+
     }
   } else {
     alert("game over !");
-    
-      document.getElementById("numbersOfTry").innerHTML = 0
+    document.getElementById("letter " + letter).disabled = true;
+    document.getElementById("numbersOfTry").innerHTML = 0
+    var x = document.getElementsByClassName("hideLetter");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.color = "#000";
+    }
+
   }
 
 }
@@ -98,6 +148,6 @@ function generateButtonsABC() {
       },
       false
     );
-   
+
   }
 }
