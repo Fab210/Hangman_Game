8cfc0d07dfc30ad1a88d2e811dc3be583d7b1c0e
Your Name <you@example.com>
2021-04-28 23:40:35 +0200

    add css animation / add modal

--- js/scripts.js
+++ js/scripts.js
@@ -7,135 +7,159 @@ var allLettersFound = 0;
 generateWordTofind();
 generateButtonsABC();
 
-
 function generateWordTofind() {
+  document.getElementById("numbersOfTry").innerHTML =
+    "Guesses Remaining :" + tryToFindLetter;
 
-  document.getElementById("numbersOfTry").innerHTML = 'Guesses Remaining :' + tryToFindLetter;
+  document.addEventListener("DOMContentLoaded", function () {
+    fetch("https://random-words-api.vercel.app/word")
+      .then((response) => response.json())
+      .then((data) => renderLetters(data));
 
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      fetch('https://random-words-api.vercel.app/word')
-        .then(response => response.json())
-        .then(data => renderLetters(data))
+    function renderLetters(data) {
+      debugger;
+      wordToFind = data[0].word;
+      wordToFind = wordToFind.toUpperCase();
+      wordDefinition = data[0].definition;
+      var wordInarray = wordToFind.split("");
 
-      function renderLetters(data) {
-        debugger
-        wordToFind = data[0].word
-        wordToFind = wordToFind.toUpperCase();
-        wordDefinition = data[0].definition;
-        var wordInarray = wordToFind.split("");
+      for (let i = 0; i < wordInarray.length; i++) {
+        const element = wordInarray[i];
 
-        for (let i = 0; i < wordInarray.length; i++) {
-          const element = wordInarray[i];
+        var generateLetters = document.createElement("div");
 
-          var generateLetters = document.createElement("div");
+        //generateLetters.id = "letter " + element;
+        generateLetters.innerText = element;
+        generateLetters.className =
+          "letter_" + element + " " + "hideLetter" + " " + "noselect";
+        generateLetters.style.borderBottomStyle = "solid";
+        generateLetters.style.borderBottomWidth = "1px";
+        generateLetters.style.borderBottomColor = "#000";
+        generateLetters.style.color = "#F2F3FB";
+        generateLetters.style.marginLeft = "6px";
 
-          //generateLetters.id = "letter " + element;
-          generateLetters.innerText = element;
-          generateLetters.className = "letter_" + element + ' ' + 'hideLetter' + ' ' + 'noselect';
-          generateLetters.style.borderBottomStyle = "solid";
-          generateLetters.style.borderBottomWidth = "1px";
-          generateLetters.style.borderBottomColor = "#000";
-          generateLetters.style.color = "#F2F3FB";
-          generateLetters.style.marginLeft = "6px";
-          
-          generateLetters.style.width = "40px";
+        generateLetters.style.width = "40px";
 
-          generateLetters.style.textAlign = "center";
-          var wordToFindDiv = document.getElementById("wordToFind");
-          wordToFindDiv.appendChild(generateLetters);
-        }
+        generateLetters.style.textAlign = "center";
+        var wordToFindDiv = document.getElementById("wordToFind");
+        wordToFindDiv.appendChild(generateLetters);
       }
     }
-  );
+  });
 }
 
 function searchLetterInWord(letter) {
-debugger
+  debugger;
   var searchLetter = wordToFind.includes(letter);
   var wordLength = wordToFind.length;
+  var animatedClasse = "animate__animated";
+  var specialAnimation = "animate__slideInDown";
   if (searchLetter == true) {
-    
     //alert("you find a letter !");
     document.getElementById("letter " + letter).disabled = true;
-    document.getElementById("letter " + letter).style.backgroundColor = "#FC7474";
+    document.getElementById("letter " + letter).style.backgroundColor =
+      "#FC7474";
     var x = document.getElementsByClassName("letter_" + letter);
     for (i = 0; i < x.length; i++) {
       x[i].style.color = "#000";
     }
-    allLettersFound = x.length + allLettersFound
-    if(wordLength === allLettersFound){
-      alert('you WON !!')
+    allLettersFound = x.length + allLettersFound;
+    if (wordLength === allLettersFound) {
+      
       disableAllButtons();
       help();
-      var p = document.createElement('p');
-      p.innerHTML ='Definition : ' + wordDefinition;
-
-      document.getElementById('definition').appendChild(p);
       
+      $(document).ready(function(){
+        
+            $("#myModal").modal('show');
+        
+      });
     }
   } else {
     //alert("no such letter found");
-    
+    debugger;
     tryToFindLetter--;
     switch (tryToFindLetter) {
       case 0:
-        alert("game over !");
+        
         document.getElementById("letter " + letter).disabled = true;
-        document.getElementById("numbersOfTry").innerHTML = 'Guesses Remaining :' + 0
+        document.getElementById("numbersOfTry").innerHTML =
+          "Guesses Remaining :" + 0;
         disableAllButtons();
         help();
         var x = document.getElementsByClassName("hideLetter");
         for (i = 0; i < x.length; i++) {
           x[i].style.color = "#000";
         }
-        document.getElementById("legR").style.visibility = "visible";
+        var legR = document.getElementById("legR");
+        legR.style.visibility = "visible";
+        legR.classList.add(animatedClasse, specialAnimation);
+        alert("game over !");
         break;
-      
+
       case 1:
-        document.getElementById("legL").style.visibility = "visible";
+        var legL = document.getElementById("legL");
+        legL.style.visibility = "visible";
+        legL.classList.add(animatedClasse, specialAnimation);
         break;
       case 2:
-        document.getElementById("armR").style.visibility = "visible";
+        var armR = document.getElementById("armR");
+        armR.style.visibility = "visible";
+        armR.classList.add(animatedClasse, specialAnimation);
         break;
       case 3:
-        document.getElementById("armL").style.visibility = "visible";
+        var armL = document.getElementById("armL");
+        armL.style.visibility = "visible";
+        armL.classList.add(animatedClasse, specialAnimation);
         break;
       case 4:
-        document.getElementById("bodyLine").style.visibility = "visible";
+        var bodyLine = document.getElementById("bodyLine");
+        bodyLine.style.visibility = "visible";
+        bodyLine.classList.add(animatedClasse, specialAnimation);
         break;
       case 5:
-        document.getElementById("head").style.visibility = "visible";
+        var head = document.getElementById("head");
+        head.style.visibility = "visible";
+        head.classList.add(animatedClasse, specialAnimation);
         break;
       case 6:
-        document.getElementById("line_4").style.visibility = "visible";
+        var line_4 = document.getElementById("line_4");
+        line_4.style.visibility = "visible";
+        line_4.classList.add(animatedClasse, specialAnimation);
         break;
-        case 7:
-        document.getElementById("line_3").style.visibility = "visible";
+      case 7:
+        var line_3 = document.getElementById("line_3");
+        line_3.style.visibility = "visible";
+        line_3.classList.add(animatedClasse, specialAnimation);
         break;
-        case 8:
-        document.getElementById("line_2").style.visibility = "visible";
-        break;
-        case 9:
-        document.getElementById("line_1").style.visibility = "visible";
-        break;
+      case 8:
+        var line_2 = document.getElementById("line_2");
+        line_2.style.visibility = "visible";
+        line_2.classList.add(animatedClasse, specialAnimation);
 
+        break;
+      case 9:
+        var line_1 = document.getElementById("line_1");
+        line_1.style.visibility = "visible";
+        line_1.classList.add(animatedClasse, specialAnimation);
+
+        break;
     }
 
-    document.getElementById("numbersOfTry").innerHTML = 'Guesses Remaining :  ' + tryToFindLetter;
+    document.getElementById("numbersOfTry").innerHTML =
+      "Guesses Remaining :  " + tryToFindLetter;
     document.getElementById("letter " + letter).disabled = true;
-    document.getElementById("letter " + letter).style.backgroundColor = "#FC7474";
+    document.getElementById("letter " + letter).style.backgroundColor =
+      "#FC7474";
   }
-
 }
 
-function disableAllButtons(){
+function disableAllButtons() {
   var y = document.getElementsByClassName("generatedButtons");
-      for (i = 0; i < y.length; i++) {
-        y[i].disabled = true;
-        y[i].style.backgroundColor = "#FC7474";
-      }
+  for (i = 0; i < y.length; i++) {
+    y[i].disabled = true;
+    y[i].style.backgroundColor = "#FC7474";
+  }
 }
 
 function generateButtonsABC() {
@@ -164,10 +188,10 @@ function generateButtonsABC() {
       false
     );
   }
-
-  
 }
 
-function help(){
-  document.querySelector('#ShowButton').innerText = wordDefinition;
+function help() {
+  document.querySelector("#ShowButton").innerText = wordDefinition;
 }
+
+
